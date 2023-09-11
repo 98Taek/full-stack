64e6fd3be9d7a24a379a645ed751c59da0a52342
Jeong Taek <imjt9345@naver.com>
2023-09-11 13:48:52 +0900

    UserManage model 추가

--- core/user/models.py
+++ core/user/models.py
@@ -8,6 +8,40 @@ from django.http import Http404
 
 # Create your models here.
 
+class UserManager(BaseUserManager):
+    def get_object_by_public(self, public_id):
+        try:
+            instance = self.get(public_id=public_id)
+            return instance
+        except (ObjectDoesNotExist, ValueError, TypeError):
+            return Http404
+
+    def create_user(self, username, email, password=None, **kwargs):
+        if username is None:
+            raise TypeError('Users must have a username.')
+        if email is None:
+            raise TypeError('Users must have an email.')
+        if password is None:
+            raise TypeError('Users must have a password.')
+        user = self.model(username=username, email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password, **kwargs):
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+        if email is None:
+            raise TypeError('Superusers must have an email.')
+        if username is None:
+            raise TypeError('Superusers must have a username.')
+        user = self.create_user(username, email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractUser, PermissionsMixin):
     public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
     username = models.CharField(db_index=True, max_length=255, unique=True)
