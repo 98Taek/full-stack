cf69c52f6c0eb8a138cc15df038b40ca034bf59d
Jeong Taek <imjt9345@naver.com>
2023-09-12 11:37:32 +0900

    router 추가

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 
 from core.auth.viewsets import LoginViewSet, RegisterViewSet
+from core.auth.viewsets.refresh import RefreshViewSet
 from core.user.viewsets import UserViewSet
 
 router = routers.SimpleRouter()
@@ -8,6 +9,7 @@ router = routers.SimpleRouter()
 router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 urlpatterns = [
     *router.urls,
 ]
