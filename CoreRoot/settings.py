e7ffae804ea8fcf9be5dd2c810604e8b1d2a52a1
Jeong Taek <imjt9345@naver.com>
2023-09-14 12:07:27 +0900

    토큰 만료시간 변경

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -145,5 +145,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'UPDATE_LAST_LOGIN': True
+    'UPDATE_LAST_LOGIN': True,
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=8),
 }
