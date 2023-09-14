22fca3868c04569bd6e634245efd6bf539d01fe7
Jeong Taek <imjt9345@naver.com>
2023-09-14 10:28:06 +0900

    post-comment 중첩 라우터 추가

--- core/routers.py
+++ core/routers.py
@@ -1,7 +1,8 @@
-from rest_framework import routers
+from rest_framework_nested import routers
 
 from core.auth.viewsets import LoginViewSet, RegisterViewSet
 from core.auth.viewsets.refresh import RefreshViewSet
+from core.comment.viewsets import CommentViewSet
 from core.post.viewsets import PostViewSet
 from core.user.viewsets import UserViewSet
 
@@ -12,6 +13,9 @@ router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 router.register(r'post', PostViewSet, basename='post')
+posts_router = routers.NestedSimpleRouter(router, r'post', lookup='post')
+posts_router.register(r'comment', CommentViewSet, basename='post-comment')
 urlpatterns = [
     *router.urls,
+    *posts_router.urls,
 ]
