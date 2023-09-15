2667f8ea2ce4b87704434eb2e3b9d7677f0aabf7
Jeong Taek <imjt9345@naver.com>
2023-09-15 13:36:37 +0900

    오타 수정

--- social-media-app/src/index.js
+++ social-media-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowerRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -9,9 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowerRouter>
+    <BrowserRouter>
       <App />
-    </BrowerRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
