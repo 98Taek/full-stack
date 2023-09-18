927f23c94d440f1f87ac246581d3a0615b1c9ba7
Jeong Taek <imjt9345@naver.com>
2023-09-18 14:42:34 +0900

    Registration 라우트 추가

--- social-media-app/src/App.js
+++ social-media-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ProtectedRoute from './routes/ProtectedRoute';
+import Registration from './pages/Registration';
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+      <Route path="/register/" element={<Registration />} />
       <Route path="/login/" element={<div>Login</div>} />
     </Routes>
   );
