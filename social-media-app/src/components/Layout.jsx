c667c7099fce170a7e782de11849e4e82ae5d02a
Jeong Taek <imjt9345@naver.com>
2023-09-21 15:17:27 +0900

    Layout Back 추가

--- social-media-app/src/components/Layout.jsx
+++ social-media-app/src/components/Layout.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Navigationbar from "./Navbar";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 function Layout(props) {
+  const { hasNavigationBack } = props;
+  const navigate = useNavigate();
+
   return (
     <div>
       <Navigationbar />
-      <div className="container m-5">{props.children}</div>
+      {hasNavigationBack && (
+        <ArrowLeftOutlined
+          style={{
+            color: "#0D6EFD",
+            fontSize: "24px",
+            marginLeft: "5%",
+            marginTop: "1%"
+          }}
+          onClick={() => navigate(-1)}
+        />
+      )}
+      <div className="container my-2">{props.children}</div>
     </div>
   );
 }
